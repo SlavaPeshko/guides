2e1769d2709ef1be24171dd0f86b8f9c2d66da2e
Slava Peshko <slava.peshko@gmail.com>
2017-10-09 14:29:25 +0300

    +added Shimming

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import _ from 'lodash';
-
 function component() {
     var element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
+    element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+    // Assume we are in the context of `window`
+    this.alert('Hmmm, this probably isn\'t a great idea...')
+
     return element;
-  }
-  
-  document.body.appendChild(component());
+}
+
+document.body.appendChild(component());
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => {
+        console.log('We retrieved some data! AND we\'re confident it will work on a variety of browser distributions.')
+        console.log(json)
+    })
+    .catch(error => console.error('Something went wrong when fetching this data: ', error))
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    polyfills: './src/polyfills.js',
+    index: './src/index.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: require.resolve('index.js'),
+        use: 'imports-loader?this=>window'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      join: ['lodash', 'join']
+    })
+  ]
 };
